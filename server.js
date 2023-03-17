a73397961206656a868cbd8f7e23648f8c352fb8
abdul mohsin <mohsin050595@gmail.com>
2023-03-17 17:17:17 +0530

    server change

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("./db.json");
-// const middlewares = jsonServer.defaults({
-//     static : "./build"
-// });
 
-// const port = process.env.PORT || 5050;
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
 
-// server.use(middlewares);
-// server.use(
-//     jsonServer.rewriter({
-//         "/api/*": "/$1",
-//     })
-// );
+const middlewares = jsonServer.defaults({
+    static: "./build",
+  });
 
-// server.use(router);
-// server.listen(port, () => {
-//     console.log(`server is running on ${port}`)
-// });
+server.use(middlewares)
+server.use(router)
+server.listen(8080, () => {
+  console.log('JSON Server is running')
+})
